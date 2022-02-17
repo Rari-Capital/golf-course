da9e4de12f1f7e0690bf757fb4d90a2032985aa3
Richie <devtooligan@gmail.com>
2022-02-16 23:56:28 -0800

    fix: add tests

---
+++ src/contracts/optimized/PayableFunctions.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.11;
+
+contract PayableFunctions {
+    /// 🚀 Optimized
+    function payableFunctions() payable external {
+    }
+
+}
